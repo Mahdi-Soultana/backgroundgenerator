2744e0bd6b40b0d31584fe509cd8bef9ddb18cd8
Mahdi-Soultana <soultana.mahdi24@gmail.com>
2020-08-14 13:47:58 -0700

    incon add and change default Background

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
-var color1=document.getElementById('color1');
-var color2=document.getElementById('color2');
-var right=document.getElementById('right');
-var left=document.getElementById('left');
-var up=document.getElementById("up");
-var down=document.getElementById('down');
-var copieButton=document.getElementById('copieButton');
-var copie=document.getElementById('copie');
-var direction="right";
+var color1 = document.getElementById('color1');
+var color2 = document.getElementById('color2');
+var right = document.getElementById('right');
+var left = document.getElementById('left');
+var up = document.getElementById("up");
+var down = document.getElementById('down');
+var copieButton = document.getElementById('copieButton');
+var copie = document.getElementById('copie');
+var direction = "right";
 
-var currentcolortext=document.getElementById("copie");
-var body=document.getElementById("body");
+var currentcolortext = document.getElementById("copie");
+var body = document.getElementById("body");
 
-function setLeft(){
-	direction="left";
+function setLeft() {
+	direction = "left";
 	setColor();
 }
-function setRight(){
-	direction="right";
+function setRight() {
+	direction = "right";
 	setColor();
 }
 
-function setColor(){
-body.style.background="linear-gradient( to "
-+direction +","+color1.value+","+color2.value+")";
-currentcolortext.value=body.style.background +";";
+function setColor() {
+	body.style.background = "linear-gradient( to "
+		+ direction + "," + color1.value + "," + color2.value + ")";
+	currentcolortext.value = body.style.background + ";";
 }
-function copieText(){
+function copieText() {
 	copie.select();
 	document.execCommand("copy");
 }
 
-color1.addEventListener("input",setColor);
-color2.addEventListener("input",setColor);
+color1.addEventListener("input", setColor);
+color2.addEventListener("input", setColor);
 
-right.addEventListener("click",setRight);
-left.addEventListener("click",setLeft);
+right.addEventListener("click", setRight);
+left.addEventListener("click", setLeft);
 
 copieButton.addEventListener("click", copieText)
-	 
+
 
